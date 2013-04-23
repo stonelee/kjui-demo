9ae3be356de4c4cdf9977144761a75d25821024d
stonelee <istonelee@gmail.com>
2013-04-23 13:20:28 +0800

    自适应高度

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -4,6 +4,29 @@ define(function(require, exports, module) {
     Tree = require('tree'),
     Switchable = require('switchable');
 
+
+  window.$ = $;
+
+  function adjustMainHeight() {
+    var mainHeight = $(window).innerHeight() - $('#header').outerHeight() - $('#footer').outerHeight();
+    $('#sub, #splitter, #container').height(mainHeight);
+  }
+
+  var menuLength;
+  function adjustMenuHeight() {
+    var $menu = $('#menu');
+    var hdHeight = $('.accordion-hd', $menu).outerHeight(),
+      titleHeight = $menu.prev().outerHeight();
+    var height = $menu.parent().innerHeight() - titleHeight - hdHeight * menuLength;
+    $('.grid-bd', $menu).height(height - 4);
+  }
+
+  $(window).resize(function() {
+    adjustMainHeight();
+    adjustMenuHeight();
+  });
+  adjustMainHeight();
+
   $.getJSON('assets/data/menu.json', function(data) {
     createAccordion(data.data.children);
 
@@ -16,17 +39,27 @@ define(function(require, exports, module) {
   });
 
   function createAccordion(data) {
+    var $menu = $('#menu');
     var html = handlebars.compile(require('./accordion.tpl'))({
       headers: data
     });
-    $('#menu').html(html);
+    $menu.html(html);
 
-    var $panels = $('#menu .accordion-bd');
+    var $panels = $('.accordion-bd', $menu);
     for (var i = 0; i < data.length; i++) {
       new Tree({
         element: $panels.eq(i),
-        data: data[i]
+        data: data[i],
+        onRendered: processTree
       }).render();
     }
+
+    function processTree(tree) {
+      tree.$('.bd').css('border-width', 0);
+    }
+
+    menuLength = data.length;
+    adjustMenuHeight();
   }
+
 });
