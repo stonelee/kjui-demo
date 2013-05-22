34434cc6a965b2dd3f8a33a3e6de42d4b1c2c5a2
stonelee <istonelee@gmail.com>
2013-05-22 13:37:05 +0800

    change with spm2

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 seajs.config({
   plugins: ['text'],
   alias: {
-    '$': 'gallery/jquery/1.8.2/jquery',
-    '$-debug': 'gallery/jquery/1.8.2/jquery-debug',
+    '$': 'jquery/jquery/1.8.0/jquery',
+    '$-debug': 'jquery/jquery/1.8.0/jquery-debug',
     handlebars: 'gallery/handlebars/1.0.0/handlebars',
-    widget: 'arale/widget/1.0.4/widget',
+    widget: 'arale/widget/1.0.3/widget',
     switchable: 'arale/switchable/0.9.12/switchable',
-    calendar: 'kjui/calendar/1.0.0/calendar',
-    datetime: 'kjui/calendar/1.0.0/datetime',
     grid: 'kjui/grid/1.3.0/grid',
     tree: 'kjui/tree/1.1.0/tree',
-    dialog: 'kjui/dialog/1.0.0/dialog'
+    dialog: 'kjui/dialog/1.0.0/dialog',
+    calendar: 'kjui/calendar/1.0.0/calendar',
+    datetime: 'kjui/calendar/1.0.0/datetime'
   }
 });
